27ec3805813e59bca807cbd35e94044cc42ff9e5
Li <li.he.7@pg.com>
2026-08-01 19:54:03 +0800

    Require auth on production while disabling cloud sync on localhost

--- cloud-config.js
+++ cloud-config.js
@@ -8,6 +8,6 @@ window.DDS_CLOUD_CONFIG = {
     anonKey: 'sb_publishable_oT0gc9Vs4xsbxWqaeQFU2Q_eV8C88HY',
     tableName: 'dds_state',
     workspaceId: 'cn-mo-qa-team-a',
-    requireAuth: false,
+    requireAuth: !ddsIsLocalDev,
     useWorkspaceColumn: true
 };
